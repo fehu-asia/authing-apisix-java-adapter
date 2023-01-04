0e41fce58192735fb1aa3212f491e10f2295f883
Gao FeiHu <fehu.asia@gmail.com>
2023-01-04 18:19:59 +0800

    first commit

--- src/main/java/cn/authing/apisix/adapter/AuthingApisixAdapterApplication.java
+++ src/main/java/cn/authing/apisix/adapter/AuthingApisixAdapterApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IpAuthApplication {
+public class AuthingApisixAdapterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IpAuthApplication.class, args);
+        SpringApplication.run(AuthingApisixAdapterApplication.class, args);
     }
 
 }
